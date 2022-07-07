e8457de1bf10436b52b3f22b0ba2fc787d4ecf2d
toymat <toyymat@gmail.com>
2022-07-07 02:18:57 +0100

    added a color

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let index = 0;
 let body = document.querySelector('body');
 
 function changeColor(){
-    let colors = ['red', 'yellow', 'blue', 'black', 'green','purple'];
+    let colors = ['red', 'yellow', 'blue', 'black', 'green','purple', 'white'];
     body.style.background = colors[index++];
 
     if(index > colors.length -1)
